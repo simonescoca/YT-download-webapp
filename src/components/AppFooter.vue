3c7f4d146698d66d7fc91cd845f24c610944d837
Simone Scoca <125935055+simonescoca@users.noreply.github.com>
2023-12-25 14:25:27 +0100

    Initial commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,46 @@
+<template>
+	<header>
+		<h3>
+			AppFooter
+		</h3>
+	</header>
+</template>
+
+<script>
+	// import {store} from "../store.js";
+	// import axios from "axios";
+
+	export default {
+		data() {
+			return {
+				// store
+			}
+		},
+
+		components: {
+
+		},
+
+		props: {
+
+		},
+
+		mounted () {
+
+		},
+
+		created () {
+
+		},
+
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+    // @use "../styles/general.scss" as *;
+	// @use "../styles/variables" as *;
+	// @use "../styles/mixins" as *;
+</style>
